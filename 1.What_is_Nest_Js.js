ce4337afc1221bd3704d81a3fe5f1ce5723b274b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-12 17:49:34 +0300

    Create 1.What_is_Nest_Js.js

---
+++ 1.What_is_Nest_Js.js
@@ -0,0 +1,101 @@
+Next js is an open-source framwork for building efficient , scalable node.js server-side applications.
+
+Nextjs was build with full support fro typescript (javascript is still supported)
+
+Combines elemnt of Object-Oriented programining , Functional programing and Reactive Programing.
+
+
+
+UNDER THE HOOD
+
+Nest js makes use of robust HTTP server frameworks (default: express.js)
+
+It provides a level abstraction above thses frameworks , which makes it incredibly joyfly to code in Typescript.
+However , it still exposes the original APIs directly to the developers
+This allows us to, for examples , use Express-specific libralies even when using nestjs
+
+
+THE PHILOSOPHY BEHINDE NEXT JS
+
+The philosophy behind Next.js revolves around simplifying the development of universal JavaScript applications, focusing on both the developer experience (DX) and user experience (UX) qed42.com. It aims to provide a hybrid framework that supports both static and dynamic websites with high performance and seamless server-side rendering.
+
+Next.js is built on React, which popularized the concept of pure render functions and component lifecycles. These concepts allow Next.js to bridge the gap between frontend and backend, making it easier to develop isomorphic applications vercel.com.
+
+Some of the key principles of Next.js philosophy include:
+
+Simplicity: Next.js provides a straightforward development experience with minimal configuration, allowing developers to focus on writing code rather than setting up the environment.
+Universal JavaScript: Next.js is built with the goal of enabling code-sharing between client and server, making it easier to develop and maintain applications.
+Performance: Next.js prioritizes performance by providing features like automatic code-splitting, server-side rendering, and static site generation, which result in fast-loading web pages.
+Developer Experience: Next.js focuses on making the development process as enjoyable and productive as possible by providing features like hot module replacement, automatic routing, and a plugin system.
+Adaptability: Next.js is a hybrid framework that can be used for building both static and dynamic websites, making it suitable for a wide range of projects.
+By adhering to these principles, Next.js has become a popular choice for developing modern web applications with high performance, scalability, and flexibility qed42.com vercel.com.
+
+
+THE PHILOSOPHY BEHINDE NEXT JS
+
+Next.js is a popular React framework for building web applications, and its philosophy revolves around a few key principles:
+
+Zero Configuration: Next.js aims to provide a streamlined development experience by minimizing configuration. It includes sensible defaults and conventions, allowing developers to start building React applications without the need for complex setup.
+
+Server-Side Rendering (SSR): Next.js focuses on server-side rendering as a default behavior. This means that the initial rendering of the application happens on the server, generating HTML that is sent to the client. SSR improves performance, allows search engines to crawl the pages, and provides a better user experience by delivering content faster.
+
+Hybrid Rendering: Next.js supports both server-side rendering and client-side rendering (CSR). It enables developers to choose the rendering strategy on a per-page basis. This hybrid rendering approach provides flexibility, allowing pages to be rendered on the server for initial load and then switch to client-side rendering for subsequent interactions.
+
+Automatic Code Splitting: Next.js automatically performs code splitting, splitting the JavaScript bundle into smaller chunks. This enables lazy loading of components and improves the application's performance by reducing the initial load time. Only the necessary code for each page is loaded, optimizing the user experience.
+
+Built-in Routing: Next.js includes built-in routing capabilities, allowing developers to define the application's routes without the need for additional routing libraries. By following file-based routing conventions, developers can create pages by simply creating React components in the pages directory, making it intuitive and easy to navigate between different views.
+
+Static Site Generation (SSG): Next.js provides static site generation, allowing you to pre-render pages at build time. This generates static HTML files for each page, which can be served directly from a content delivery network (CDN). SSG enables high performance, reduces server load, and ensures consistent content delivery.
+
+Extensibility and Ecosystem: Next.js is highly extensible and integrates well with other libraries and frameworks. It has a vast ecosystem of plugins, modules, and community-contributed packages that extend its capabilities. This allows developers to leverage existing tools and libraries to enhance their Next.js applications.
+
+Overall, Next.js embraces simplicity, performance, and developer experience. It combines the benefits of server-side rendering, automatic code splitting, hybrid rendering, and built-in routing to provide a powerful framework for building scalable and optimized React applications. Its philosophy revolves around making modern web development accessible and efficient while ensuring a great user experience.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
