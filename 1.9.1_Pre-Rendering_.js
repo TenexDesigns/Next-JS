83b3a4dc18b934bb18a7d995c7aade95f4f15b3b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-01 09:19:13 +0300

    Create 1.9.1_Pre-Rendering_.js

---
+++ 1.9.1_Pre-Rendering_.js
@@ -0,0 +1,36 @@
+React vs NextJs
+By default , Next  js pre-renders every page in the application
+
+What does pre-rendering mean
+Pre-Rending means -Next js generates HTML for  each page in adavance instead of having it all done by client side javascript.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
