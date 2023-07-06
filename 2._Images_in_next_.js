0d40929633165a9033e7737386f437b116454480
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-06 12:41:57 +0300

    Update 2._Images_in_next_.js

--- 2._Images_in_next_.js
+++ 2._Images_in_next_.js
@@ -27,7 +27,7 @@ Style the Image component using Tailwind CSS classes:
   height={300}
   className="rounded-lg"
 />
-In this example, we've added the rounded-lg class to round the corners of the image.
+In this example, weve added the rounded-lg class to round the corners of the image.
 
 By combining next/image with Tailwind CSS classes, you can easily style and optimize your images in a Next.js application. Remember to configure and customize the next/image package according to your project's specific requirements, such as image formats, quality settings, and caching strategies. You can refer to the Next.js documentation for more details on working with the next/image package.
 
@@ -37,42 +37,41 @@ By combining next/image with Tailwind CSS classes, you can easily style and opti
 WORKING WITH IMAGES -ERROR INCURED WITH NEXT JS
 ********************************************************************************************8
 
-When using Next.js, the next/image package provides an optimized and performant way to handle images in your application. It is recommended to use next/image instead of the standard <img> tag for various reasons:
+Youre encountering two separate issues here:
 
-Advantages of using next/image in Next.js:
+Local Images: In Next.js, the public directory is the root directory for serving static files. So, when referring to an image in the public directory,
+  you dont need to include /public in the path.
+Change your image URLs like this:
 
-Image Optimization: next/image automatically optimizes and delivers images in the most efficient format and size based on the device, resulting in faster loading times and improved performance.
+<Card title="Card 1" imageUrl="/img/curry.jpg" description="This is card 1." />
+<Card title="Card 2" imageUrl="/img/noodles.jpg" description="This is card 2." />
+External Images: For external images, Next.js 10 introduced a new Image component that requires additional configuration for loading images from external hosts.
+  Youll need to add the external host to the images property in your next.config.js file.
+If you dont have a next.config.js file yet, create one in the root directory of your project.
 
-Responsive Images: With next/image, you can easily generate and serve responsive images based on different screen sizes and resolutions without manual resizing and cropping.
+Heres how to add Pexels to your allowed image hosts:
 
-Automatic Image Optimization: Next.js provides built-in image optimization using tools like sharp and lqip, allowing you to automatically optimize images during the build process.
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.pexels.com'],
+  },
+}
 
-To write your Tailwind CSS code using next/image, you can follow these steps:
-
-Import the next/image component in your component file:
-javascript
-Copy code
-import Image from 'next/image';
-Use the Image component instead of the standard <img> tag:
-javascript
-Copy code
-<Image src="/path/to/image.jpg" alt="Image" width={500} height={300} />
-In the above example, replace /path/to/image.jpg with the actual path to your image file. Specify the width and height props to set the dimensions of the image.
-
-Style the Image component using Tailwind CSS classes:
-javascript
-Copy code
-<Image
-  src="/path/to/image.jpg"
-  alt="Image"
-  width={500}
-  height={300}
-  className="rounded-lg"
-/>
-In this example, we've added the rounded-lg class to round the corners of the image.
-
-By combining next/image with Tailwind CSS classes, you can easily style and optimize your images in a Next.js application. Remember to configure and customize the next/image package according to your project's specific requirements, such as image formats, quality settings, and caching strategies. You can refer to the Next.js documentation for more details on working with the next/image package.
+module.exports = nextConfig
 
 
 
+Please note that the URL you provided (https://www.pexels.com/photo/men-hiking-on-mountain-14207351/) is a webpage URL, not a direct image URL.
+You need to use the direct image URL (usually ending in .jpg, .png, etc.) for the src prop in next/image. You can usually find this by right-clicking the 
+  image on the webpage and selecting "Copy image address" fullstacksoup.blog.
+
+
+
+
+
+
+
+    
 
