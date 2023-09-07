9dbbfdbb283e775d7880a4525cf71c5aca913463
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-07 14:07:17 +0300

    Create Images_and_assets.java

---
+++ UI/Images_and_assets.java
@@ -0,0 +1,41 @@
+There are several websites and resources where you can find pre-made UI assets, including images, icons, SVGs, and more, for free. Here are some popular options:
+
+1. **Unsplash (Images):** Unsplash offers a vast collection of high-quality, royalty-free images that you can use for various purposes, including web design and UI projects.
+
+   Website: [Unsplash](https://unsplash.com/)
+
+2. **Pexels (Images and Videos):** Pexels provides a wide range of free stock photos and videos that you can use in your projects without attribution.
+
+   Website: [Pexels](https://www.pexels.com/)
+
+3. **Icons8 (Icons):** Icons8 offers a large library of free icons in various styles and formats. You can search and download icons for both personal and commercial use.
+
+   Website: [Icons8](https://icons8.com/)
+
+4. **FontAwesome (Icons):** FontAwesome provides a free collection of icons and icon fonts that you can use in your web projects. They also offer a paid Pro version with additional icons and features.
+
+   Website: [FontAwesome](https://fontawesome.com/)
+
+5. **Flaticon (Icons):** Flaticon offers a vast collection of free icons, including SVGs and PNGs. It provides customizable icon packs for different themes and styles.
+
+   Website: [Flaticon](https://www.flaticon.com/)
+
+6. **SVGRepo (SVGs):** SVGRepo is a repository of SVG files that you can download and use in your web projects. It offers a wide variety of SVG graphics.
+
+   Website: [SVGRepo](https://www.svgrepo.com/)
+
+7. **Material Icons (Icons):** Google's Material Icons library provides a set of icons in the Material Design style, available for free. You can easily integrate them into your web projects.
+
+   Website: [Material Icons](https://material.io/resources/icons/)
+
+8. **Freepik (Graphics and Icons):** Freepik offers a mix of free and premium graphic resources, including vectors, icons, illustrations, and more. While some assets require attribution, many are available for free use.
+
+   Website: [Freepik](https://www.freepik.com/)
+
+9. **OpenGameArt (Game Assets):** If you're working on game development or interactive applications, OpenGameArt provides a collection of free game assets, including sprites, textures, and sound effects.
+
+   Website: [OpenGameArt](https://opengameart.org/)
+
+10. **GitHub (Open Source):** You can also find UI assets and resources on GitHub repositories, especially in open-source projects. Many developers and designers share their work on GitHub under permissive licenses.
+
+Remember to check the licensing terms and attribution requirements for each resource you use to ensure compliance with their usage policies. Always give proper credit when required by the licensing terms, especially for assets that are provided for free.
