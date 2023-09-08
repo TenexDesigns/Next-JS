d83b9b1fc7c295ad1e0c04f979e08e5204a84dd9
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-08 15:17:31 +0300

    Create Grocery_app.js

---
+++ UI/Grocery_app/Grocery_app.js
@@ -0,0 +1,269 @@
+import Image from "next/image"
+
+function Home() {
+  return (
+<div>
+
+
+<main className=" section">
+<section>
+<div className="  relative px-24  pb-20 py-5 container  min-w-full bg-[url('/img/image.png')] bg-center bg-cover">
+
+
+<nav className="flex items-center">
+    <Image className="  w-20 sm:w-24  md:w-36 cursor-pointer " src="/img/logo.png" height="200" width="300"></Image>
+    <ul className="  flex-1 text-center">
+    <li className=" px-4 text-white list-none inline-block">Home</li>
+    <li className=" px-4 text-white list-none inline-block">About</li>
+    <li className=" px-4 text-white list-none inline-block">Features</li>
+    <li className=" px-4 text-white list-none inline-block">Contact</li>
+    </ul>
+
+
+
+    <Image className="h-4 w-4" src="/img/cart.png" width="40" height="10"></Image>
+</nav>
+
+
+
+<div className=" mt-36 text-white">
+  <h1 className=" text-6xl font-semibold leading-normal">
+    Grocery <br/> delivery in  <span className=" font-light">15 mins</span> </h1>
+
+  <p className="max-w-lg">
+    Grocfy offers a wude range of products , 
+    including fress products, meats, dairly, 
+    bake goods and non-perishable items.
+  </p>
+
+
+
+
+<div className=" flex flex-row   mt-10">
+
+<button className="mr-6">
+  <a href="#" className=" text-black bg-yellow-300  rounded-xl p-3">Order now</a>
+
+
+  </button>
+  
+
+ <button className=" mr-6">
+ <a href="#" className=" text-black bg-[#F7FF3D] rounded-xl p-3">Download app</a>
+
+  </button> 
+
+
+  
+</div>
+
+
+<div className="mt-10">
+ <a href="#" className=" inline-block mr-4 text-black bg-yellow-300 rounded-full py-3 px-5 font-medium hover:bg-red-400 hover:border hover:border-solid duration-300  ">Download app</a>
+
+ <a href="#" className=" text-black bg-[#F7FF3D] rounded-xl p-3">Download app <span className="text-lg">&#8594;</span></a>
+
+</div>
+
+
+</div>
+
+
+<div className="w-full md:w-1/2 absolute bottom-0  right-8  ">
+<img src="img/grocery-image.png" className="  w-auto bg-cover "/>
+
+
+</div>
+
+<img
+    src="img/grocery-image.png"
+    class="hidden md:block w-auto md:w-1/2 bg-cover absolute bottom-0 right-8"
+    alt="Grocery Image"
+  />
+
+
+</div>
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+<section className="bg bg-green-400">
+          section two
+          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>
+          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+            </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>
+        </section>
+
+
+
+        <section className="bg bg-red-400">
+          section two
+          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>
+          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+            </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>          <div>
+            Mpj
+          </div>
+        </section>
+
+
+        <section>
+          <div>Threee</div>
+        </section>
+
+
+
+       </main>
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Home
