98daeb1604d264c0cf04f20536b57cd9f69a70f7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-13 15:59:37 +0300

    Create Tailwind Components.java

---
+++ 1_UI_Projects_To_Emulate/Tailwind Components.java
@@ -0,0 +1,3 @@
+
+Tailwind Components: This is a repository of user-submitted Tailwind CSS components and templates. You can browse a wide range of components and templates,
+all of which are free to use and customize  find out more on https://tailwindcomponents.com/.
