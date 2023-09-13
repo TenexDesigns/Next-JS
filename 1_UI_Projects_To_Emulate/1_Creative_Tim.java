63dee05df4307554d379edfdd47b7e6008b90207
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-13 15:56:13 +0300

    Create 1_Creative_Tim.java

---
+++ 1_UI_Projects_To_Emulate/1_Creative_Tim.java
@@ -0,0 +1,40 @@
+Sure, here are some resources where you can find free and customizable Tailwind CSS templates:
+
+Tailwind Starter Kit by Creative Tim: This is a beautiful extension for Tailwind CSS, and it's free to use. It doesn't
+  change or add any CSS to the already one from Tailwind CSS. It features multiple HTML and JS elements, 
+  and it comes with dynamic components for ReactJS, Vue, and Angular Find out more on https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
+
+
+
+
+
+Tailwind CSS provides a wide range of pre-designed, open-source templates that you can use as a starting point for your projects. Here are a few examples of popular, free, and customizable Tailwind CSS templates:
+
+Tailwind Starter Kit by Creative Tim:
+
+Repository: https://github.com/creativetimofficial/tailwind-starter-kit
+Description: A beautifully designed, open-source Tailwind CSS starter kit with multiple example pages.
+Features: Landing page, pricing page, contact page, and more.
+Live Demo: https://www.creative-tim.com/learning-lab/tailwind-starter-kit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
