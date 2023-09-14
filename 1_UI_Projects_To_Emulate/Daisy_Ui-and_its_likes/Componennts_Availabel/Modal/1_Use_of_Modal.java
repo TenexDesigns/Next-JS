d6b2eeeae476f6e211ee2c0a59902ac0fedc9ab0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-14 20:45:44 +0300

    Create 1_Use_of_Modal.java

---
+++ 1_UI_Projects_To_Emulate/Daisy_Ui-and_its_likes/Componennts_Availabel/Modal/1_Use_of_Modal.java
@@ -0,0 +1,91 @@
+In a Next.js application, you can use the DaisyUI Modal component to create a modal dialog with an input and a submit button. Here's an example of how to create such a modal:
+
+```jsx
+import { useState } from 'react';
+
+function MyComponent() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+  const handleChange = (e) => setInputValue(e.target.value);
+  const handleSubmit = () => {
+    // Submit your data here...
+    console.log(inputValue);
+    closeModal();
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+
+      {isOpen && (
+        <div className="modal modal-open">
+          <div className="modal-box">
+            <h2 className="text-2xl">My Modal</h2>
+            <input
+              className="input input-bordered w-full"
+              value={inputValue}
+              onChange={handleChange}
+            />
+            <div className="modal-action">
+              <button onClick={handleSubmit} className="btn">Submit</button>
+              <button onClick={closeModal} className="btn">Close</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+```
+[Source 5](https://nextui.org/docs/components/modal)
+
+In this example, the `useState` hook is used to manage the state of the modal (whether it's open or not) and the input value. The `openModal` and `closeModal` functions are used to open and close the modal, respectively. The `handleChange` function is used to update the input value when the user types into the input field. The `handleSubmit` function is where you would put your code to submit the data (for example, making an API call).
+
+As for updating the page data without reloading in Next.js, you can use a combination of state management and the `useEffect` hook. 
+
+Here's an example of how you could do this:
+
+```jsx
+import { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from your API
+    fetchData().then(setData);
+  }, []);
+
+  const refreshData = async () => {
+    const newData = await fetchData();
+    setData(newData);
+  };
+
+  // ...
+}
+```
+[Source 16](https://dev.to/robbiegm/how-to-reload-a-next-js-page-s-initial-props-without-reloading-the-whole-page-3oid)
+
+In this example, the `useState` hook is used to manage the state of the data. The `useEffect` hook is used to fetch the data when the component mounts. The `refreshData` function is used to fetch the new data and update the state, which causes the component to re-render with the new data.
+
+You can call the `refreshData` function whenever you want to refresh the data, for example, after submitting the form in the modal.
+
+Remember to replace `fetchData` with your actual function to fetch data from your API.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
