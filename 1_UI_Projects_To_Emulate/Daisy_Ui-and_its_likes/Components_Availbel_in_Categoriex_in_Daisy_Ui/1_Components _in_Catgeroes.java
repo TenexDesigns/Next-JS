ddeb71d653cc36537ebe004be102ab3dd68100ce
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-15 19:50:20 +0300

    Update 1_Components _in_Catgeroes.java

--- 1_UI_Projects_To_Emulate/Daisy_Ui-and_its_likes/Components_Availbel_in_Categoriex_in_Daisy_Ui/1_Components _in_Catgeroes.java
+++ 1_UI_Projects_To_Emulate/Daisy_Ui-and_its_likes/Components_Availbel_in_Categoriex_in_Daisy_Ui/1_Components _in_Catgeroes.java
@@ -2,36 +2,65 @@
 
 
 ACTIONS
-  Button
-  Dropdown
-  Modal
-  Swap
+      Button
+      Dropdown
+      Modal
+      Swap
 
 Data Display
-  Accoedion
-  Allert
-  Avatar
-  Badge
-  Card
-  Carouseel
-  ChatBubble
-  Collapse
-  CountDown
-  Kbd
-  Loading
-  Progress
-  Stat
-  Table
-  Tooltip
+      Accoedion
+      Allert
+      Avatar
+      Badge
+      Card
+      Carouseel
+      ChatBubble
+      Collapse
+      CountDown
+      Kbd
+      Loading
+      Progress
+      Stat
+      Table
+      Tooltip
 
 
 
   DATA INPUT
-  Checkbox
-  File input
-  Radio
-  Range
-  Ratting
+      Checkbox
+      File input
+      Radio
+      Range
+      Rating
+      Select
+      Text input
+      Text area
+      Toogle
+
+
+  Layout
+      Divider
+      Drawer
+      Footer
+      Hero
+      Indicator
+      Join (Group Items)
+      Mask
+      Stack
+      Toast
+
+
+
+  Navigation
+
+  breadcrumbs
+  Bottom Naviagtion
+  Link
+  Menu
+  Navbar
+  Pagination
+  Steps
+  Tab
 
 
 
