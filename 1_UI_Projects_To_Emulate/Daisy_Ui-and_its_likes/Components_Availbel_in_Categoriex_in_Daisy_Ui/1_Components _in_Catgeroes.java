23addcea9e547eb176d705955e0aa2f55a67eb87
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-15 18:36:20 +0300

    Create 1_Components _in_Catgeroes.java

---
+++ 1_UI_Projects_To_Emulate/Daisy_Ui-and_its_likes/Components_Availbel_in_Categoriex_in_Daisy_Ui/1_Components _in_Catgeroes.java
@@ -0,0 +1,76 @@
+
+
+
+ACTIONS
+  Button
+  Dropdown
+  Modal
+  Swap
+
+Data Display
+  Accoedion
+  Allert
+  Avatar
+  Badge
+  Card
+  Carouseel
+  ChatBubble
+  Collapse
+  CountDown
+  Kbd
+  Loading
+  Progress
+  Stat
+  Table
+  Tooltip
+
+
+
+  DATA INPUT
+  Checkbox
+  File input
+  Radio
+  Range
+  Ratting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
