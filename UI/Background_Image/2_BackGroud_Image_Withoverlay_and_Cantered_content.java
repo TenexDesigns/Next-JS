bfd8648c77645c15d9eba2ec0fb90d8191115a27
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-29 17:54:23 +0300

    Create 2_BackGroud_Image_Withoverlay_and_Cantered_content.java

---
+++ UI/Background_Image/2_BackGroud_Image_Withoverlay_and_Cantered_content.java
@@ -0,0 +1,27 @@
+
+
+
+
+    <div className=" h-[100vh] w-full bg-cover relative bg-no-repeat bg-center flex items-center justify-center" 
+    style={{ backgroundImage: "url('img/testimonial.jpg')" }}> 
+ 
+ 
+ <div className='absolute top-0 bottom-0 left-0 right-0 bg-[#083051] opacity-30 flex flex-row justify-center items-center'>
+ 
+</div>
+
+
+      <div> George </div>
+ 
+
+</div>
+
+
+
+
+
+
+
+
+
+      
