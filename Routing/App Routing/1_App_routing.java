21135f61f652b45585d913d23ef382b7ba45635d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-13 16:18:16 +0300

    Update 1_App_routing.java

--- Routing/App Routing/1_App_routing.java
+++ Routing/App Routing/1_App_routing.java
@@ -1,3 +1,107 @@
+app routing in next js , fearures it provides such as, routing folder based routing inseted of file nased like in pages routing, page.tsxt () to wrap related routes, folder then pages .tsx insted of inex.js. 
+  reciving parametes using [slog], and other in app routing in next js
+
+
+
+
+
+
+
+---------------------------------------------------------------------------------------------------------------------
+
+In Next.js, app routing provides several features that enhance the routing capabilities of your application. Here are some of the features it provides:
+
+1. Folder-based Routing: Next.js allows you to organize your routes using folder-based routing instead of file-based routing. This means that you can create a folder with the same name as the route and place the related files inside that folder. For example, you can create a folder named `blog` and place the files related to the blog routes inside that folder.
+
+2. `index.js` as Default: By default, Next.js treats `index.js` as the default file for a route. So, if you have a folder named `blog` with an `index.js` file inside it, it will be treated as the default route for the `/blog` path.
+
+3. Custom Page Extensions: Next.js allows you to use custom file extensions for your pages. Instead of using `.js` or `.tsx` extensions, you can use `.page.js` or `.page.tsx` extensions for your page files. This can help you differentiate between regular JavaScript/TypeScript files and Next.js page files.
+
+4. Dynamic Routing: Next.js supports dynamic routing, which allows you to create routes with dynamic parameters. You can define dynamic routes by using square brackets `[]` in the file name. For example, if you have a file named `[slug].js`, it will match any route with a parameter named `slug`.
+
+5. Accessing Parameters: To access the parameters in dynamic routes, you can use the `useRouter` hook from the `next/router` module. The parameters will be available in the `query` object of the `router` object. For example, if you have a dynamic route with the file name `[slug].js`, you can access the `slug` parameter using `router.query.slug`.
+
+6. Nested Routes: Next.js allows you to create nested routes by organizing your files and folders accordingly. For example, you can have a folder structure like `pages/blog/[slug].js` to create nested routes for blog posts.
+
+These features provide flexibility and organization in defining and handling routes in your Next.js application. They allow you to create clean and structured routing patterns, handle dynamic parameters, and organize your files and folders in a logical manner.
+
+
+
+
+
+
+
+
+
+
+
+
+  ---------------------------------------------------------------------------------------------------------------------
+
+
+
+
+App routing in Next.js offers several advantages over the traditional file-based routing approach used in pages routing. Here are some of the key features and benefits of app routing:
+
+1. **Folder-Based Routing:** App routing utilizes folders to organize routes instead of relying on individual files. This structure is more intuitive and scalable for complex applications with multiple routes.
+
+2. **Page Layouts:** App routing enables the creation of reusable page layouts that can be applied to multiple routes. This promotes consistent UI design and reduces code duplication.
+
+3. **Dynamic Routing:** App routing supports dynamic routes using URL patterns with parameter placeholders, allowing for flexible URLs and data-driven content rendering.
+
+4. **Nested Routes:** App routing facilitates nested routes, enabling you to create hierarchical navigation structures with parent-child relationships between routes.
+
+5. **Data Fetching Integration:** App routing integrates seamlessly with data fetching strategies, allowing for data loading and rendering based on route transitions.
+
+6. **Error Handling:** App routing provides mechanisms for handling errors and displaying custom error pages, ensuring a graceful user experience in case of unexpected issues.
+
+Here are some specific examples of how app routing features are used in Next.js:
+
+1. **Folder-Based Routing:** Instead of having individual files for each route, you can create folders that contain the route component and any related components or data. This makes it easier to manage and organize routes for larger applications.
+
+2. **Page Layouts:** You can define a layout component in a separate file and then apply it to multiple routes. This ensures that all routes have a consistent look and feel, reducing the need for repetitive code.
+
+3. **Dynamic Routing:** Use URL patterns with parameter placeholders to match dynamic URLs. For example, a route with the pattern `/blog/:slug` can match any URL like `/blog/my-post` or `/blog/another-post`, and the `slug` parameter will contain the corresponding post identifier.
+
+4. **Nested Routes:** Create nested routes to define hierarchical navigation structures. For instance, a route with the pattern `/dashboard/settings` can be nested within a route with the pattern `/dashboard`, allowing you to organize routes based on their relationship.
+
+5. **Data Fetching Integration:** Use `getStaticProps` or `getServerSideProps` to fetch data for a route before rendering the page. This ensures that the page has all the necessary data before it's displayed to the user.
+
+6. **Error Handling:** Create custom error pages to handle navigation errors or rendering errors. This provides a more informative and user-friendly experience when something goes wrong.
+
+App routing is a powerful and flexible approach to navigation in Next.js, offering a more structured and organized way to manage routes, handle dynamic URLs, and create consistent UI experiences. It's particularly well-suited for larger and more complex applications where traditional file-based routing may become cumbersome.
+
+
+
+
+
+  ---------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 In Next.js, you can implement app routing using the `next-routes` library. `next-routes` allows you to define custom routes and handle dynamic routing in your Next.js application. Here's an example of how to use `next-routes` for app routing in Next.js:
 
 1. Install the `next-routes` package:
