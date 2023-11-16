49887857bb11df1c7e4f4119b857368f72e73184
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-16 21:19:53 +0300

    Create 3_Learn react.java

---
+++ Typescript with Next js/React/Learn and Become Good in React/3_Learn react.java
@@ -0,0 +1,36 @@
+Learning React and becoming proficient in building scalable and complex applications involves a combination of theoretical learning, practical application, and continuous learning. Here are some resources, projects, and libraries that can help you on this journey:
+
+**Resources:**
+
+1. **Courses**: There are many online courses that can help you learn React. Some of the best ones include:
+  - [React for Beginners Course](https://www.udemy.com/course/react-for-beginners/) on Udemy: This course emphasizes readability and simplicity, making it one of the best ways to advance your JavaScript skills with React. It covers essential topics like real-time web sockets, application state, UI components, writing markup in JSX, URL routing, deploying your React app, and more [Source 0](https://hackr.io/blog/react-courses).
+  - [React for Beginners: Build an APP and Learn the Fundamentals](https://www.skillshare.com/classes/React-for-Beginners-Build-an-APP-and-Learn-the-Fundamentals/1798023416) on Skillshare: This course is taught by Ryan Johnson, a full stack developer in Node.js and React. It's suitable for React Beginners and includes 2 projects [Source 1](https://kinsta.com/blog/best-react-tutorials/).
+  - [React JS Full Course 2022](https://www.javascriptmastery.com/react-js-full-course-2022/) on JavaScript Mastery: This course is 1 hour long and contains fundamental explanations and instructions on how to read and comprehend the documentation [Source 1](https://kinsta.com/blog/best-react-tutorials/).
+
+2. **Books**: Some of the best books for learning React include:
+  - "Learning React" by Alex Banks and Eve Porcello: This book is a comprehensive guide to building web applications with React.
+  - "The Road to React" by Robin Wieruch: This book is a great resource for beginners and covers everything from the basics of React to more advanced topics.
+
+3. **Libraries**: Some of the most popular libraries used with React include:
+  - Redux: A predictable state container for JavaScript apps.
+  - React Router: A collection of navigational components that compose declaratively with your application.
+  - Material-UI: A popular React UI framework with a set of React components that implement Google's Material Design.
+
+**Projects:**
+
+Building projects is a great way to learn and practice React. Here are some project ideas:
+
+1. **Weather App**: A weather app that displays the current temperature and forecast for a given location. This project will help you understand how to fetch data from an API and display it in your React application [Source 21](https://lokeshkavisth.hashnode.dev/the-ultimate-guide-to-becoming-a-react-expert).
+
+2. **Recipe App**: A recipe app that allows users to search for and save recipes. This project will help you understand how to manage state in a React application and how to work with forms and user input [Source 21](https://lokeshkavisth.hashnode.dev/the-ultimate-guide-to-becoming-a-react-expert).
+
+3. **Blog**: A blog where users can create, read, update, and delete blog posts. This project will give you a good understanding of how to work with a backend in a React application [Source 19](https://www.freecodecamp.org/news/31-steps-to-become-a-professional-react-developer/).
+
+4. **Online Store**: An online store where users can add products to a cart, view the cart, and make a purchase. This project will help you understand how to manage complex state and how to handle user interactions [Source 3](https://medium.com/javarevisited/6-best-websites-to-learn-react-js-coding-for-free-ba7ec5c43433).
+
+Remember, the key to becoming proficient in React is to keep practicing and building projects. Don't be afraid to experiment and try new things. Good luck on your journey to becoming a React developer!
+
+
+
+
+  ....
