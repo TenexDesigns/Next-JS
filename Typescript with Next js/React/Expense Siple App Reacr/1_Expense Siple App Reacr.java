68586430bc194bf1158f5e4e16997e168b83269b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-16 21:28:40 +0300

    Update 1_Expense Siple App Reacr.java

--- Typescript with Next js/React/Expense Siple App Reacr/1_Expense Siple App Reacr.java
+++ Typescript with Next js/React/Expense Siple App Reacr/1_Expense Siple App Reacr.java
@@ -157,6 +157,84 @@ export default ExpenseList;
 
 
 
+FORM IN EXPENS
+-------------------------------------------------------------------------------
+  
+
+import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  age: z.number().positive({ message: "Age must be a positive number" }),
+});
+interface FormData {
+  name: string;
+  age: number;
+}
+
+function Form2() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        onSubmit(data);
+        reset();
+      })}
+    >
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name")}
+          type="text"
+          className={`form-control ${errors.name ? "is-invalid" : ""}`}
+        />
+        {errors.name && (
+          <div className="invalid-feedback">{errors.name.message}</div>
+        )}
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age")}
+          type="number"
+          className={`form-control ${errors.age ? "is-invalid" : ""}`}
+        />
+        {errors.age && (
+          <div className="invalid-feedback">{errors.age.message}</div>
+        )}
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default Form2;
+
+
+
 
 
 
