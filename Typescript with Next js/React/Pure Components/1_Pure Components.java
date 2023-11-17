255dff8265ffecfb06d69bbdeeef0d62f632d1c0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-17 16:57:29 +0300

    Update 1_Pure Components.java

--- Typescript with Next js/React/Pure Components/1_Pure Components.java
+++ Typescript with Next js/React/Pure Components/1_Pure Components.java
@@ -18,3 +18,8 @@ To keep components pure , keep chanages out of the render phases
 
 
   React exutes effect after a render, and to make effect hook excute again, we can use a depency, which will recute the code in the useEffce hook if the depency chnages, the change can be caues by a change in satet of th depency
+
+
+
+  Code Clean up
+  - In the clean up our functions should undo or stop what ever whas happening in the effect hook , e.g  timer, connection to server, if subscribing, our clean up should unsubscribe, if our effect is feching data from server , in clean up it should abort the fetch 
