5601187b89ea19c98c44f13bfea7201b658e7362
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-17 16:43:20 +0300

    Create 1_Pure Components.java

---
+++ Typescript with Next js/React/Pure Components/1_Pure Components.java
@@ -0,0 +1,11 @@
+To keep components pure , keep chanages out of the render phases
+
+
+  Side effects
+  - Store data in local storage
+  - Call the server to fetch/save data
+  - Manualy modify the dom
+
+
+  UseEffect
+  - To excute a piece of code after a component has rendred
