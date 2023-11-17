b1ef3c71b34f571c7deca004031c2e971fff3453
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-17 16:49:21 +0300

    Update 1_Pure Components.java

--- Typescript with Next js/React/Pure Components/1_Pure Components.java
+++ Typescript with Next js/React/Pure Components/1_Pure Components.java
@@ -9,3 +9,9 @@ To keep components pure , keep chanages out of the render phases
 
   UseEffect
   - To excute a piece of code after a component has rendred
+ - After render
+
+  We can have multiple useEffect, react will excute them in order
+
+
+  -React renders each component because of stric mode in development
