cbe87f72298b4a7f41de70989c108e6665e3d069
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-21 16:20:55 +0300

    Create 2_React.java

---
+++ Typescript with Next js/Material Ui/1_ Install/1_React/2_React.java
@@ -0,0 +1,98 @@
+To get started with Material-UI in a React TypeScript Vite project, you can follow these steps:
+
+### 1. Create a React TypeScript Vite Project:
+
+Use the following commands to create a new React TypeScript Vite project:
+
+```bash
+npm create vite my-material-ui-app --template react-ts
+cd my-material-ui-app
+```
+
+### 2. Install Material-UI Packages:
+
+Install the core Material-UI package and the necessary dependencies:
+
+```bash
+npm install @mui/material @emotion/react @emotion/styled
+```
+
+If you want to use Material-UI icons, you can also install the icons package:
+
+```bash
+npm install @mui/icons-material
+```
+
+### 3. Create a Theme File:
+
+Create a file for your theme configuration. For example, you can create a `theme.ts` file in your `src` directory:
+
+```typescript
+// src/theme.ts
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
+
+export default theme;
+```
+
+### 4. Integrate Material-UI Provider:
+
+Wrap your `src/index.tsx` file (or `src/main.tsx` for Vite projects) with the Material-UI `ThemeProvider` and provide your theme:
+
+```tsx
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+```
+
+### 5. Use Material-UI Components:
+
+Now, you can use Material-UI components in your React TypeScript Vite app. For example, you can modify the `App.tsx` file:
+
+```tsx
+// src/App.tsx
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Material-UI in Vite!</h1>
+      <Button variant="contained" color="primary">
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
+```
+
+### 6. Run Your Vite App:
+
+Run your Vite app to see Material-UI in action:
+
+```bash
+npm run dev
+```
+
+Visit `http://localhost:3000`
+
+
+
+  ...
